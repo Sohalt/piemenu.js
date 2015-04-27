17e51a9c004c4470cc7433450b1930205afcd843
sohalt <github@sohalt.net>
2015-04-27 22:14:28 +0200

    add demo

---
+++ demo.js
@@ -0,0 +1,16 @@
+var slices = [];
+for(var i = 0; i < 8; i++){
+    (function (k){
+        var c = "even";
+        if(k%2==1){
+            c = "odd";
+        };
+        slices[k] = {
+            trigger:function(){console.log("slice " + k + " triggered")},
+            "class":c,
+        };
+    })(i);
+}
+
+
+contextPie(slices,{radius:70,innerRadius:20});
